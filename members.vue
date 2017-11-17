5650d67fe5c524e3db18ad3eb93303f1ffcb35f9
Jose <jakermater@gmail.com>
2017-11-17 13:48:43 +0100

    last one

---
+++ members.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>{{ name }}</h3>
+        <img v-bind:src="image" :alt="name" /> 
+        <p>{{ superpower }}</p>
+    </div>
+</template>
+
+
+<script>
+// import otroComponente from './otroComponente'
+export default {
+    name:'alea-members', // name of the tag
+    // components: { otroComponente}
+    props: {
+        name: String,
+        superpower: String,
+        image: String
+    }   
+}
+</script>
+
+
+<style scoped>
+    img {
+        width:100;
+    }
+
+</style>
