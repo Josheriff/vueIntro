23241c89ad580b00e9b49513c0cbbd447f7ed47a
Jose <jakermater@gmail.com>
2017-11-17 13:47:02 +0100

    now the next

---
+++ little.js
@@ -0,0 +1,33 @@
+// Añadiendo para directiva FOR
+Vue.component('alea-members',{
+    props:['name','superpower','image'], // los parametros del componente
+    template:`
+        <div>
+            <h3>{{ name }}</h3>
+            <img v-bind:src="image" :alt="name" /> 
+            <p>{{ superpower }}</p>
+        </div>
+    ` 
+    // image tiene que ir con v-bind porque es un atributo de un html, ESTÁ DENTRO
+});
+
+new Vue({
+    el:'#app',
+    data:{
+         members:[
+             {name:'Montse',superpower:'Super System Administrator',image:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Binary_Code.jpg/800px-Binary_Code.jpg'},
+             {name:'APA',superpower:'Super trustworthy ',image:'https://ae01.alicdn.com/kf/HTB1VHr9OFXXXXcXapXXq6xXFXXXJ/2017-nueva-laptop-sticker-vinyl-decal-bob-esponja-pokonyan-parcial-de-la-piel-para-macbook-air.jpg'},
+             {name:'Raul',superpower:'Super Quick starter',image:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Binary_Code.jpg/800px-Binary_Code.jpg'}
+         ]
+    },
+    template:`
+    <div>
+    <alea-members v-for="(member,index) in members"
+    :name="member.name"
+    :superpower="member.superpower" 
+    :image="member.image"
+    >   
+    </alea-members>
+    </div>
+    `
+})
